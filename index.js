0a429e794ce87f87fd5b8a232ddc600ff401e078
Luis Eduardo Altino <ads.luis.sobrinho@gmail.com>
2023-02-17 18:49:32 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Client, LocalAuth} = require('whatsapp-web.js');
+const { Configuration, OpenAIApi } = require("openai");
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth({ clientId: "whatsapp-gpt" })
+});
+
+const configuration = new Configuration({
+    apiKey: '',
+});
+const openai = new OpenAIApi(configuration);
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('authenticated', (session) => {
+    console.log('Client is authenticated!');
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async (message) => {
+    if(message.body.toLowerCase().startsWith('papai')) {
+        console.log(`Message: ${message.body.slice(5)}`)
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: message.body.slice(5),
+            max_tokens: 4000,
+        });
+        console.log(`Response GTP ${response.data.id}`, response.data)
+        for (const choice of response.data.choices) {
+            console.log(choice.text)
+            await message.reply(choice.text);
+        }
+    }
+});
+
+void client.initialize();
